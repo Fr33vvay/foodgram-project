da25e27fed4af8d2195a13ddbba66c2b2b0d1dda
Илья <7634216@gmail.com>
2020-12-23 20:45:01 +0300

    поле электронной почты обязательно

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-
+from django import forms
 User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
+    """Форма регистрации пользователя"""
+
+    email = forms.EmailField(required=True, label='Электронная почта')
+
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('first_name','username', 'email')
