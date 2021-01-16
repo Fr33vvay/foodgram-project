f8153711a3be4fbfff9f333818a722f2be6eeafb
Илья <7634216@gmail.com>
2021-01-17 01:00:07 +0300

    работа с валидатором ингредиентов

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Ingredient
 
 
 class RecipeForm(ModelForm):
@@ -14,3 +14,19 @@ class RecipeForm(ModelForm):
             'description',
             'image',
         )
+
+    # def clean_ingredient(self):
+    #     """Валидатор для ингридиентов"""
+    #     ingredient_names = self.data.getlist('nameIngredient')
+    #     ingredient_dimension = self.data.getlist('unitsIngredient')
+    #     ingredient_quantity = self.data.getlist('valueIngredient')
+    #     ingredients_clean = []
+    #     for ingredient in zip(ingredient_names, ingredient_dimension,
+    #                           ingredient_quantity):
+    #         if Ingredient.objects.filter(title=ingredient[0]).exists():
+    #             ingredients_clean.append({'title': ingredient[0],
+    #                                       'dimension': ingredient[1],
+    #                                       'quantity': ingredient[2]})
+    #     if len(ingredients_clean) == 0:
+    #         raise forms.ValidationError('Добавте ингридиент')
+    #     return ingredients_clean
--- recipes/validators.py
+++ recipes/validators.py
@@ -5,4 +5,21 @@ def validate_file(fieldfile_obj):
     filesize = fieldfile_obj.file.size
     megabyte_limit = 2.0
     if filesize > megabyte_limit * 1024 * 1024:
-        raise ValidationError(f'Максимальный размер файла - 2 Мб')
+        raise ValidationError('Максимальный размер файла - 2 Мб')
+
+
+# def validate_ingredient(self):
+#         """Валидатор для ингридиентов"""
+#         ingredient_names = self.data.getlist('nameIngredient')
+#         ingredient_dimension = self.data.getlist('unitsIngredient')
+#         ingredient_quantity = self.data.getlist('valueIngredient')
+#         ingredients_clean = []
+#         for ingredient in zip(ingredient_names, ingredient_dimension,
+#                               ingredient_quantity):
+#             if Ingredient.objects.filter(title=ingredient[0]).exists():
+#                 ingredients_clean.append({'title': ingredient[0],
+#                                           'dimension': ingredient[1],
+#                                           'quantity': ingredient[2]})
+#         if len(ingredients_clean) == 0:
+#             raise forms.ValidationError('Добавте ингридиент')
+#         return ingredients_clean
